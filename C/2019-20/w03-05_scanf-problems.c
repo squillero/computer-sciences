46f186205edec9bb0353064451a2b7fe67c49f90
Giovanni Squillero <giovanni.squillero@polito.it>
2019-10-15 11:21:05 +0200

    First programs

---
+++ C/2019-20/w03-05_scanf-problems.c
@@ -0,0 +1,24 @@
+/********************************************************************-*-c-*-*\
+*               *  Code example for Computer Sciences 2019-20                *
+*    #####      *  (!) Giovanni Squillero <giovanni.squillero@polito.it>     *
+*   ######      *                                                            *
+*   ###   \     *  Copying and distribution of this file, with or without    *
+*    ##G  c\    *  modification, are permitted in any medium without royalty *
+*    #     _\   *  provided this notice is preserved.                        *
+*    |   _/     *  This file is offered as-is, without any warranty.         *
+*    |  _/      *                                                            *
+*               *  See: http://staff.polito.it/giovanni.squillero/dida.php   *
+\****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int yeuch;
+    scanf("The answer is %d", &yeuch);
+    printf("Yeuch = %d\n", yeuch);
+
+    return 0;
+}
