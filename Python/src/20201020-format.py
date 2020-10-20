8a0c4bceca26cb22e2aa988a9321298d5886fc7a
Giovanni Squillero <giovanni.squillero@polito.it>
2020-10-20 13:10:05 +0200

    Format strings

--- Python/src/20201020-format.py
+++ Python/src/20201020-format.py
@@ -1,6 +1,14 @@
-# CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)
-# (!) 2020, Giovanni Squillero <squillero@polito.it>
-# https://github.com/squillero/computer-sciences
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
 
 foo = 4.2
 
