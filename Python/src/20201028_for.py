016792f6c6c5b2a50c13fe303ac09b5eb76051c1
Giovanni Squillero <giovanni.squillero@polito.it>
2020-10-28 13:08:40 +0100

    Add lecture of October, 28th

---
+++ Python/src/20201028_for.py
@@ -0,0 +1,18 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+DIM = 12
+
+for n1 in range(1, DIM+1):
+    for n2 in range(1, DIM+1):
+        print(f"{n1*n2:4d}", end='')
+    print()
