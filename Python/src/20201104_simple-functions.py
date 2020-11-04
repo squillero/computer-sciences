ee609f82359d389c0ad2455c0dc3a8c80a43d073
Giovanni Squillero <giovanni.squillero@polito.it>
2020-11-04 13:31:10 +0100

    Add lecture of Wednesday, November 4th
    Former-commit-id: e37cd7703989c487e77f689c5f14af04c2623200 [formerly 187128f0716f7aa62b45ef6cb4c52724dedd4870]
    Former-commit-id: 31d5ad39f0305bd52ab469de8fcfe619c142c1f8

---
+++ Python/src/20201104_simple-functions.py
@@ -0,0 +1,25 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+def draw_line(size):
+    print("#" * size)
+
+def main():
+    foo = int(input("Square size: "))
+    draw_square(foo)
+
+def draw_square(size):
+    for l in range(size):
+        draw_line(size)
+
+main()
+
