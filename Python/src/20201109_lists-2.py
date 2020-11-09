c6e4b1bf8a752dbca62c9a6291863e68a077edad
Giovanni Squillero <giovanni.squillero@polito.it>
2020-11-09 17:33:14 +0100

    Add lecture of Monday, November 9th

---
+++ Python/src/20201109_lists-2.py
@@ -0,0 +1,21 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+def main():
+    beatles = ['John', 'Paul', 'George', 'Ringo']
+    print("The BEATLES:")
+    for i in range(4):
+        print(f"I love Beatles number {i+1}: {beatles[i]}!")
+
+
+if __name__ == '__main__':      # BLACK MAGIC!
+    main()
