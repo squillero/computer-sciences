91063a244788c18d3d46cc3b5a91cb2a416820db
Giovanni Squillero <giovanni.squillero@polito.it>
2020-11-25 13:03:48 +0100

    Add lecture of Wednesday, Novermer 11th
    Former-commit-id: ba19a115ec4e2a7a3f3831214259c0e5a57a91c8 [formerly 910613c149c0a4c8b7da65f9d7217ebf754c2d40]
    Former-commit-id: f803b335464f178dd39945fb6d52845a4f8436ad

---
+++ Python/src/20201125_convert-B2B_1 (partial).py
@@ -0,0 +1,80 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+
+def main():
+    """Main entry point"""
+    number = input("Number: ")
+    base_from = int(input("Base: "))
+    base_to = int(input("Base: "))
+
+    if base_from == 10:
+        value = int(number)
+    else:
+        value = convert10(number)
+
+    if base_to == 10:
+        result = str(value)
+    else:
+        result = convert_to_generic_base(value, base_to)
+
+    print(f"{number}|{base_from} = {result}|{base_to}")
+
+
+def convert_to_generic_base(number, base):
+    """Conver a number in base10 to a different base (return a str)"""
+    result = ''
+    while number > 0:
+        result = result + hexadecimal_digit(number % base)
+        number = number // base
+    if result == '':
+        result = '0'        # !!!
+    return result[::-1]
+
+
+def hexadecimal_digit(val):
+    if val == 0:
+        return "0"
+    elif val == 1:
+        return "1"
+    elif val == 2:
+        return "2"
+    elif val == 3:
+        return "3"
+    elif val == 4:
+        return "4"
+    elif val == 5:
+        return "5"
+    elif val == 6:
+        return "6"
+    elif val == 7:
+        return "7"
+    elif val == 8:
+        return "8"
+    elif val == 9:
+        return "9"
+    elif val == 10:
+        return "A"
+    elif val == 11:
+        return "B"
+    elif val == 12:
+        return "C"
+    elif val == 13:
+        return "D"
+    elif val == 14:
+        return "E"
+    elif val == 15:
+        return "F"
+
+
+if __name__ == '__main__':
+    main()
