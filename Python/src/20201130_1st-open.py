9be7207a17d4797a878db80c1c9fab783642ec0f
Giovanni Squillero <giovanni.squillero@polito.it>
2020-11-30 17:34:18 +0100

    Add lecture of Monday, november 30th
    Former-commit-id: e55f36f6b54dc853aabe352bab58de88f5e9536d [formerly 4a47aaee72d37d344f7e15a365f35ab0f5dbe9ae]
    Former-commit-id: 27e793092374b67ef14f0db891ac5a69961665ea

---
+++ Python/src/20201130_1st-open.py
@@ -0,0 +1,26 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+
+def main():
+    file = open("duck.txt", "r")
+    line = file.readline()
+    line = file.readline()
+    while line != '':
+        print(f"LINE: <<<{line[:-1]}>>>")
+        line = file.readline()
+    file = ""
+    file.close()
+
+
+if __name__ == '__main__':
+    main()
