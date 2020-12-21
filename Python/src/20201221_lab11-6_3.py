b9fe5fbaf2c2b39d710322013e34d947fe73b907
Giovanni Squillero <giovanni.squillero@polito.it>
2020-12-21 17:34:44 +0100

    Add lecture of Monday, December 21st

---
+++ Python/src/20201221_lab11-6_3.py
@@ -0,0 +1,57 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+FILE_URL = 'https://www.cia.gov/library/publications/the-world-factbook/rankorder/rawdata_2004.txt'
+
+import urllib.request as req
+
+
+def main():
+    """Entry point"""
+
+    countries = dict()
+    try:
+        for raw_line in req.urlopen(FILE_URL):
+            line = raw_line.decode('utf-8').rstrip()
+            first_block, income = line.rsplit(maxsplit=1)
+            line_number, country = first_block.split(maxsplit=1)
+            value = int(income[1:].replace(',', ''))
+            countries[country] = value
+    except OSError as problem:
+        print(f"Yeuch, we have a problem: {problem}")
+
+    # not sorted (ie. creation order)
+    for k in countries:
+        print(f"{k} -> {countries[k]}")
+    print()
+
+    # sorted in alphabetic order
+    for k in sorted(countries):
+        print(f"{k} -> {countries[k]}")
+    print()
+
+    # sorted according to the value
+    from operator import itemgetter
+    for k in sorted(countries, key=itemgetter(1), reverse=True):
+        print(f"{k} -> {countries[k]}")
+    print()
+
+    # sorted according to the value (but using a lambda)
+    for k in sorted(countries, key=lambda k: countries[k], reverse=True):
+        print(f"{k} -> {countries[k]}")
+    print()
+
+
+
+
+if __name__ == '__main__':
+    main()
