8482da15ea3033a1aaff1c5b734ab54877314ef9
Giovanni Squillero <giovanni.squillero@polito.it>
2020-12-21 17:34:26 +0100

    Add lecture of Wednesday, December 16th
    Former-commit-id: 1cc133dfc486a4bd43a2783ab28baa5e0d505d29 [formerly 26e384fcf268868ed8057d6970d9b43ac21b439a]
    Former-commit-id: 4246404b898e89064f12a292ba5ebb63adedb9ce

---
+++ Python/src/20201216_modules_gargle.py
@@ -0,0 +1,14 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+def blaster():
+    print("PAN GALACTIC GARGLE BLASTER!")
---
+++ Python/src/20201216_modules_main.py
@@ -0,0 +1,23 @@
+##--------------#-----------------------------------------------------------#
+#|              | CLASS SAMPLE FOR "COMPUTER SCIENCES" (07JCJ**)            #
+#|   ######     | (!) 2020, Giovanni Squillero <squillero@polito.it>        #
+#|  #######     | https://github.com/squillero/computer-sciences            #
+#|  ####   \    |                                                           #
+#|   ###G  c\   | => Copying and distributing this file for classroom use,  #
+#|   ##     _\  | either with or without modification, is permitted without #
+#|   |    _/    | royalties provided that this 11-line comment is preserved #
+#|   |   _/     |                                                           #
+#|              | => THIS FILE IS OFFERED AS-IS, WITHOUT ANY WARRANTY       #
+##--------------#-----------------------------------------------------------#
+
+
+import gargle
+from gargle import blaster as yoda
+
+def main():
+    """Entry point"""
+    gargle.blaster()
+    yoda()              # same as before
+    
+if __name__ == '__main__':
+    main()
