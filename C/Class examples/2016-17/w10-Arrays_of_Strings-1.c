eee000d8b0d6bb58da3145b60f37308383e6e5ef
Giovanni Squillero <giovanni.squillero@polito.it>
2021-09-04 16:05:36 +0200

    Correct typo
    Former-commit-id: d29677e5747ddfe8845cc65304de75c1cb7f8ed0 [formerly a5e8be4462742d43b2ca8e81aebd68a20f434bf6]
    Former-commit-id: c85ab73ab0ff2aa3479e911477197e1b1bf2216b

--- C/Class examples/2016-17/w10-Arrays_of_Strings-1.c
+++ C/Class examples/2016-17/w10-Arrays_of_Strings-1.c
@@ -1,6 +1,6 @@
 /*  ######       /******************************************************\
 |*  #######      * CLASS EXAMPLE FOR "COMPUTER SCIENCES" (04JCJ**)      *
-|*  ####   \     * https://github.com/squillero/computer-science        *
+|*  ####   \     * https://github.com/squillero/computer-sciences       *
 |*   ###G  c\    *                                                      *
 |*   ##     _\   * Copyright © Giovanni Squillero <squillero@polito.it> *
 |*   |    _/     * Licensed under the EUPL-1.2 <https://eupl.eu/>       *
