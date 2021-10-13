3588ad0d00227a7b069a9d1f08fca1642d032d09
Giovanni Squillero <giovanni.squillero@polito.it>
2021-10-13 13:28:14 +0200

    Add lecture of Wednesday, October 13th

---
+++ Python/src/2021-22/20211013_tiles.py
@@ -0,0 +1,21 @@
+#    ######       /=================================================\
+#   #######       | CLASS EXAMPLE FOR "COMPUTER SCIENCES" (07JCJ**) |
+#   ####   \      | https://github.com/squillero/computer-sciences  |
+#    ###G  c\     |                                                 |
+#    ##     _\    | © 2021 Giovanni Squillero <squillero@polito.it> |
+#    |    _/      | Free for personal or classroom use.             |
+#    |   _/       \=================================================/
+
+TILE_SIZE = 5
+
+# Change the following line and run
+total_width = 105
+
+usable_width = total_width - TILE_SIZE
+num_pairs = usable_width // (2 * TILE_SIZE)
+tile_width = TILE_SIZE + 2 * num_pairs * TILE_SIZE
+total_gap = total_width - tile_width
+gap = total_gap / 2
+
+print(f"With a wall of {total_width}cm we can use {1+2*num_pairs} tiles")
+print(f"corresponding to {tile_width}cm and we need a gap of {gap}cm each end")
