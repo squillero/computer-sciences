a2191c9f8b44be123eb0f0fb309e7e63e55f2859
Giovanni Squillero <giovanni.squillero@polito.it>
2021-10-28 04:42:18 +0200

    Add lecture of Wednesday, October 27th

---
+++ Python/src/2021-22/20211027_for-loop.py
@@ -0,0 +1,16 @@
+#    ######       /=================================================\
+#   #######       | CLASS EXAMPLE FOR "COMPUTER SCIENCES" (07JCJ**) |
+#   ####   \      | https://github.com/squillero/computer-sciences  |
+#    ###G  c\     |                                                 |
+#    ##     _\    | © 2021 Giovanni Squillero <squillero@polito.it> |
+#    |    _/      | Free for personal or classroom use.             |
+#    |   _/       \=================================================/
+
+num = int(input(": "))
+
+for n in range(1_000):
+    if n % num == 0:
+        print(n)
+
+for n in range(, 1_000, num):
+    print(n)
