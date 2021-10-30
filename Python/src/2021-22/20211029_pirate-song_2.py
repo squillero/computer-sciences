696183659d5b4481cd292cc2dad785b8df5d019a
Giovanni Squillero <giovanni.squillero@polito.it>
2021-10-30 12:33:48 +0200

    Add a simple 1-line solution
    Former-commit-id: 01651b999e774b8185cb1d178d52e80b9f86f761 [formerly f9528a2f7cd611ae85ab050e83d366e63dfe1666]
    Former-commit-id: c9a58e593ba9e712abf16f3f88f6fcc564c6eb7e

--- Python/src/2021-22/20211029_pirate-song_2.py
+++ Python/src/2021-22/20211029_pirate-song_2.py
@@ -5,8 +5,9 @@
 # string = input()
 string = 'VanaTallinn'
 
-x = list()
-for num_chars in range(1, len(string) + 1):
-    for i1 in range(len(string) - num_chars + 1):
-        x.append(string[i1:i1 + num_chars + 1])
-print("\n".join(sorted(x)))
+# A 1-line solution using list comprehnesion (and a generator)
+
+print("\n".join(string[a:a + b]
+                for b in range(1,
+                               len(string) + 1)
+                for a in range(len(string) - b + 1)))
