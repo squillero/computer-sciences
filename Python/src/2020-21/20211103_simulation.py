475ae995e4d8ba9ceeedf31d46389229677c4735
Giovanni Squillero <giovanni.squillero@polito.it>
2021-11-03 15:17:40 +0100

    Add lecture of Wednesday, Noveber 11th
    Former-commit-id: 66ac8a84f08828cc092d445ae5e1d9c074d37aa1 [formerly ed7b620d788782b26a851aeaee58f8772afe8c49]
    Former-commit-id: 12de789187d9309d50237b2b9910275c30d8d4db

---
+++ Python/src/2020-21/20211103_simulation.py
@@ -0,0 +1,27 @@
+# Copyright © 2021 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+# Is 7 really more probable with 2 dices?
+
+NUM_TOSSES = 1_000_000
+DICE_LOW = 1
+DICE_HIGH = 6
+
+import random
+
+n2 = n3 = n7 = 0
+
+for t in range(NUM_TOSSES):
+    d1 = random.randint(DICE_LOW, DICE_HIGH)
+    d2 = random.randint(DICE_LOW, DICE_HIGH)
+
+    if d1 + d2 == 2:
+        n2 += 1
+    elif d1 + d2 == 3:
+        n3 += 1
+    elif d1 + d2 == 7:
+        n7 += 1
+
+print(
+    f"2's {n2/NUM_TOSSES*100:.2f}% -- 3's {n3/NUM_TOSSES*100:.2f}% -- 7's {n7/NUM_TOSSES*100:.2f}%")
