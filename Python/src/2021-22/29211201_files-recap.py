d16adf45da963e6b419eace715389adc77bbd001
Giovanni Squillero <giovanni.squillero@polito.it>
2021-12-01 13:12:04 +0100

    Add lecture of Wednesday, December 1st
    Former-commit-id: e35bae763d125572f1deb07b6e8790663dd386a5 [formerly 67620bc8562a139a4f543d4db26c72a95128c614]
    Former-commit-id: 05adb0751be64f05a396eb94dc38091ece3f5897

---
+++ Python/src/2021-22/29211201_files-recap.py
@@ -0,0 +1,29 @@
+# Copyright © 2021 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+FILENAME = "20211201_song.txt"
+
+
+def get_words(file_name):
+    with open(file_name) as file:
+        whole_file = file.read()
+    words = whole_file.split()
+    return words
+
+
+def read_file(file_name):
+    with open(file_name) as file:
+        # whole_text = file.read()  # slurps the whole file into a single string
+        # whole_text = file.readlines() # returns a list with all the lines as element
+        # for line in file.readlines(): # terrible: I'm reading the whole file
+        for line in file:  # correct: reading 1 line at a time
+            line = line.rstrip('\n')
+            print(f">>>{line}<<<")
+
+
+def main():
+    print(get_words(FILENAME))
+
+
+main()
