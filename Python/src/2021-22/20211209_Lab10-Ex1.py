b4c9b4780bcba552f5f55e08c3bad912819d2844
Giovanni Squillero <giovanni.squillero@polito.it>
2021-12-09 14:00:37 +0100

    Add lab of December, 9th

---
+++ Python/src/2021-22/20211209_Lab10-Ex1.py
@@ -0,0 +1,20 @@
+# Copyright © 2021 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+INPUT_FILE = '20211209_input.gx'
+OUTPUT_FILE = '20211209_output.gx'
+
+
+def main():
+    try:
+        with open(INPUT_FILE) as input_, open(OUTPUT_FILE, 'w') as output:
+            for index, line in enumerate(input_):
+                output.write(f"/*{index+1}*/ {line}")
+    except OSError as error:
+        print(f"Yeuch, we had a problem: {error}")
+
+
+if __name__ == '__main__':
+    main()
+
