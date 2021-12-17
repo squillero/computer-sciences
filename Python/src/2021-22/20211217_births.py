20974cb68166e7094660938657d71f6d6f96af13
Giovanni Squillero <giovanni.squillero@polito.it>
2021-12-17 13:06:59 +0100

    Add lecture of Friday, December 17th
    Former-commit-id: a97dca275694b7ec153f26d99d3c41338238b78c [formerly 564d99ab150314b8d71bc703e131ea87133e5b77]
    Former-commit-id: 520236505eab5e202fcaae54797c8b6d65c19188

---
+++ Python/src/2021-22/20211217_births.py
@@ -0,0 +1,52 @@
+# Copyright © 2021 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+FILE_NAME = '20211218_births.txt'
+MONTH_NAMES = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
+}
+
+
+def count_non_zero(seq):
+    nz = 0
+    for e in seq:
+        if e != 0:
+            nz += 1
+    return nz
+
+
+def main():
+    month_births = [0] * 12
+
+    try:
+        with open(FILE_NAME) as input_file:
+            for line in input_file:
+                name, surname, place, date_raw = line.split()
+                day, month, year = date_raw.split('/')
+                day = int(day)
+                month = int(month) - 1
+                year = int(year)
+                month_births[month] += 1
+    except OSError as problem:
+        print(f"Yeuch, we have a problem: {problem}")
+        exit(1)
+    max_month = month_births.index(max(month_births))
+    print(f"The month with the maximum number of births: {MONTH_NAMES[max_month]}")
+    print(
+        f"Average number of births per month: {sum(month_births)/count_non_zero(month_births):.2f}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ Python/src/2021-22/20211217_maze.py
@@ -0,0 +1,46 @@
+# Copyright © 2021 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+from pprint import pprint
+
+FILE_NAME = '20211217_maze.txt'
+MAZE_WALL = '*'
+MAZE_CORRIDOR = ' '
+
+
+def read_maze(filename):
+    maze = list()
+    with open(filename) as input_file:
+        for line in input_file:
+            maze.append(list(line[:-1]))
+    return maze
+
+
+def is_corridor(maze, row, column):
+    if row < 0 or row >= len(maze):
+        return False
+    elif column < 0 or column >= len(maze[row]):
+        return False
+    elif maze[row][column] == MAZE_WALL:
+        return False
+    elif maze[row][column] == MAZE_CORRIDOR:
+        return True
+
+
+def main():
+    maze = read_maze(FILE_NAME)
+    pprint(maze)
+    corridors = dict()
+    for row in range(len(maze)):
+        for column in range(len(maze[row])):
+            if is_corridor(maze, row, column):
+                corridors[(row, column)] = set()
+                for r, c in [(0, -1), (0, +1), (-1, 0), (+1, 0)]:
+                    if is_corridor(maze, row + r, column + c):
+                        corridors[(row, column)].add((row + r, column + c))
+    pprint(corridors)
+
+
+if __name__ == '__main__':
+    main()
