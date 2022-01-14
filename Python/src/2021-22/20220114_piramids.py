b4edf16c10a2f6762e6009090d92d13b6fdcfabc
Giovanni Squillero <giovanni.squillero@polito.it>
2022-01-14 13:23:43 +0100

    Add final lecture (Friday, January 14th 2022)

---
+++ Python/src/2021-22/20220114_piramids.py
@@ -0,0 +1,44 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+from pprint import pprint
+
+MAP_FILENAME = '20220114_map.txt'
+
+
+def peak(map_, row, col):
+    for r, c in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        try:
+            if map_[row + r][col + c] != map_[row][col] - 1:
+                return False
+        except IndexError:
+            pass
+    return True
+
+
+def main():
+    map_ = read_map(MAP_FILENAME)
+    for row in range(len(map_)):
+        for col in range(len(map_[0])):
+            if peak(map_, row, col):
+                print(f"{map_[row][col]} {row} {col}")
+
+
+def read_map(filename):
+    map_ = list()
+    try:
+        with open(filename) as file_in:
+            for line in file_in:
+                values = list()
+                for v in line.split():
+                    values.append(int(v))
+                map_.append(values)
+    except OSError as problem:
+        print(f"Yeuch: {problem}")
+        exit(0)
+    return map_
+
+
+if __name__ == '__main__':
+    main()
---
+++ Python/src/2021-22/20220114_recipes.py
@@ -0,0 +1,60 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+import sys
+from csv import reader
+
+INGREDIENTS_FILENAME = '20220114_ingredients.txt'
+RECIPE_FILENAME = '20220114_polenta_concia.txt'
+
+
+def read_ingredients(filename):
+    ingredients = dict()
+    try:
+        with open(filename, newline='') as data:
+            for d in reader(data, delimiter=';'):
+                ingredients[d[0].strip()] = {'Cost': float(d[1]), 'Calories': int(d[2])}
+    except OSError as problem:
+        print(f"Yeuch: {problem}")
+        exit(1)
+    return ingredients
+
+
+def read_recipe(filename):
+    ingredients = dict()
+    try:
+        with open(filename, newline='') as data:
+            line = data.readline()
+            line = data.readline().rstrip()
+            while line:
+                ing, qty = line.split(';')
+                ingredients[ing.strip()] = {'Quantity': float(qty)}
+                line = data.readline().rstrip()
+    except OSError as problem:
+        print(f"Yeuch: {problem}")
+        exit(1)
+    return ingredients
+
+
+def main():
+    available_ingredients = read_ingredients(INGREDIENTS_FILENAME)
+    required_ingredients = read_recipe(RECIPE_FILENAME)
+    print("Ingredients:")
+    cost = 0
+    calories = 0
+    for ing in sorted(required_ingredients,
+                      key=lambda i: required_ingredients[i]['Quantity'],
+                      reverse=True):
+        print(f"{ing} - {required_ingredients[ing]['Quantity']:.1f}")
+        cost += available_ingredients[ing]['Cost'] / 1000 * required_ingredients[ing]['Quantity']
+        calories += available_ingredients[ing]['Calories'] / 1000 * required_ingredients[ing][
+            'Quantity']
+    print()
+    print(f"Number of ingredients: {len(required_ingredients)}")
+    print(f"Recipe cost: {cost:.2f}")
+    print(f"Recipe calories: {calories:.2f}")
+
+
+if __name__ == '__main__':
+    main()
