2047a6befea69843e1d2da9c59eaf311344a3a2b
Giovanni Squillero <giovanni.squillero@polito.it>
2022-07-02 09:10:41 +0200

    Add first exam of summer 2022
    Former-commit-id: 826ecd50c6dfe164d8613ff7394fd0a30f49ea0c [formerly 6809af2cf43fb0bb7b4a24d24b0dd9e758b088ed]
    Former-commit-id: 0f0e517d45978ce54b0709689b7af4926df52114

---
+++ Python/exams/armstrong/main.py
@@ -0,0 +1,24 @@
+min# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# Free for personal or classroom use; see 'LICENCE.md' for details.
+# https://github.com/squillero/computer-sciences
+
+NUMBERS = 'numbers.txt'
+AMSTRONG_NUMBERS = 'armstrong.txt'
+
+
+def armstrong(number):
+    return number == sum([int(d)**len(str(number)) for d in str(number)])
+
+
+def main():
+    try:
+        with open(NUMBERS) as fin, open(AMSTRONG_NUMBERS, 'w') as fout:
+            for number in fin.readlines():
+                if armstrong(int(number)):
+                    fout.write(f"{int(number)}\n")
+    except OSError as err:
+        print(f"Yeuch: {err}")
+
+
+if __name__ == "__main__":
+    main()
