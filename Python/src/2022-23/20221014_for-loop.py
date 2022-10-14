c46e862634f689f4c3ee6e1bb646f042c489e1fb
Giovanni Squillero <giovanni.squillero@polito.it>
2022-10-14 18:23:06 +0200

    Update

--- Python/src/2022-23/20221014_for-loop.py
+++
@@ -1,8 +0,0 @@
-# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
-# https://github.com/squillero/computer-sciences
-# Free for personal or classroom use; see 'LICENSE.md' for details.
-
-name = "Giovanni Adolfo Pietro Pio"
-
-for elena in name:
-    print("Hey!")
