7b02119be4ed4eca3771c3e8c9485b1cc4351fd6
Giovanni Squillero <giovanni.squillero@polito.it>
2022-10-14 17:44:59 +0200

    First Python lecture
    Former-commit-id: 8cbd352b1f7dfd9426eaf178f083cd2881d9b404 [formerly e44e941a7330ad22bd4886b4a131d31042358afe]
    Former-commit-id: ab54737c74987c09043d66b44ef02968140a7f6a

---
+++ Python/src/2022-23/20221011_math.py
@@ -0,0 +1,18 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+#foo = math.sqrt(3.2)
+#print(foo)
+
+import math
+import cmath
+
+foo = math.sqrt(3.2)
+print(foo)
+
+foo = cmath.sqrt(-3.2)
+print(foo)
+
+foo = cmath.sqrt(3.2)
+print(foo)
