24464930713cdc9f0242a5f7611dc466216b872f
Giovanni Squillero <giovanni.squillero@polito.it>
2022-11-02 17:52:35 +0100

    Add lecture of October, 28th
    Former-commit-id: 89f3bdeb9306e1e0af606a8be818ea16f223d736 [formerly b8b364535dc9be3a5efb977de47357702f29cde8]
    Former-commit-id: dd83b7dd98d953eff46445f3572eb37524e8b424

---
+++ Python/src/2022-23/20221028-nim_1-2.py
@@ -0,0 +1,43 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+from random import randint
+from math import log2
+
+
+def nim_smart(marbles):
+    return marbles-2**int(log2(marbles))+1
+
+
+def nim_dumb(marbles):
+    if marbles == 1:
+        return 1
+    else:
+        return randint(1, marbles//2)
+
+
+def main():
+    marbles = randint(10, 100)
+    print(marbles)
+
+    if randint(0, 1) == 0:
+        nim = nim_smart
+    else:
+        nim = nim_dumb
+
+    player = randint(0, 1)
+    while marbles > 0:
+        print(f"{marbles} remaining")
+        if player == 0:
+            ply = nim(marbles)
+        else:
+            ply = int(input("Remove: "))
+        print(f"Player {player} removes {ply} marbles")
+        marbles = marbles - ply
+        player = 1 - player
+    print(f"Player {player} won!")
+
+
+if __name__ == '__main__':
+    main()
