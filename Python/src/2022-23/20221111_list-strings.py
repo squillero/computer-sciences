6a3b638170ebf4fe52931d870c2618fd993f86c3
Giovanni Squillero <giovanni.squillero@polito.it>
2022-11-11 14:20:01 +0100

    Add lecture of Friday, November 11th
    Former-commit-id: e16f2f81da611870e411425f56237b49dbdfa041 [formerly c17f9831a0ebf75c49520e92fa212ffefdb05aa4]
    Former-commit-id: 5659f31d117b0ff712102fa2623fcdac1b0eba82

---
+++ Python/src/2022-23/20221111_list-strings.py
@@ -0,0 +1,29 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+
+def main():
+    monkeys = ['Alex Turner', 'Jamie Cook', 'Matt Helders', 'Nick O\'Malley']
+    band = ''
+    for member in monkeys[:-1]:
+        band = band + member + ', '
+    band = band + monkeys[-1]
+    print(band)
+
+    strokes = [
+        'Julian Casablancas', 'Nikolai Fraiture', 'Albert Hammond Jr.', 'Fabrizio Moretti',
+        'Nick Valensi'
+    ]
+
+    # use 'join' from str
+    band = str.join(', ', strokes)
+    print(band)
+
+    # equivalent! oo-like
+    band = ', '.join(strokes)
+    print(band)
+
+
+if __name__ == '__main__':
+    main()
