9b29c562339bd0f3d87be540a9531bffd9f7e6e4
Giovanni Squillero <giovanni.squillero@polito.it>
2022-11-25 13:48:48 +0100

    Car Park & Zip
    Former-commit-id: 6c6ee1035b3d5c076d27e85add2d2aa158bcdec1 [formerly 4310a1b6fc831aaf0a02b7af165a21a9a400698e]
    Former-commit-id: c23a3219c51875de91049e4a52b3300a80a95f75

---
+++ Python/src/2022-23/20221125_cmp-lists.py
@@ -0,0 +1,32 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+DATA = [1, 3, 5, 7, 9, 11, 23, 45]
+#DATA = [1, 3, -5, 7, 9, 11, 23, 45]
+
+
+def main():
+    """Check if all element of an array are non decreasing"""
+
+    # giovanni-giulio's algorithm
+    if DATA == sorted(DATA):
+        print("Yeah!")
+
+    nd = True
+    for i in range(len(DATA)-1):
+        if DATA[i] > DATA[i+1]:
+            nd = False
+    if nd:
+        print("Yeah!")
+
+    nd = True
+    for l1, l2 in zip(DATA, DATA[1:]):
+        if l1 > l2:
+            nd = False
+    if nd:
+        print("Yeah!")
+
+
+if __name__ == '__main__':
+    main()
