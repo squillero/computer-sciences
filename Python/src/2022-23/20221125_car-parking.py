ed85a46ceda1de7e86a2faa55f975e4a75a166e1
SwaggyP <redafakih.99@gmail.com>
2022-11-27 14:26:20 +0200

    Changed the ParkingLot solution to much simpler recursion solution (Reda Fakih)

--- Python/src/2022-23/20221125_car-parking.py
+++ Python/src/2022-23/20221125_car-parking.py
@@ -2,38 +2,37 @@
 # https://github.com/squillero/computer-sciences
 # Free for personal or classroom use; see 'LICENSE.md' for details.
 
-def park2list(parking):
-    """Convert parking [False, False, False, ...] to
-    list of tuples"""
-    slots = list()
-    end = 0
-    start = 0
-    while end < len(parking):
-        if parking[end]:
-            slots.append((start, end-start))
-            start = end+1
-        end += 1
-    else:
-        slots.append((start, end - start))
-    return slots
+# Solution proposed by Reda Fakih 308192
 
+def PrintParking(parkingSlots : list):
+    temp = ""
+    for space in parkingSlots:
+        if space:
+            temp += 'x'
+        else:
+            temp += '_'
+    
+    print(temp)
 
-def get_max_slot(slots):
-    m = slots.pop(0)
-    for s in slots:
-        if s[1] > m[1]:
-            m = s
-    return m
+def ParkCars(spaces : list, l : int, r : int):
+    if l > r:
+        return
+
+    median = (l + r) // 2
+    spaces[median] = 1
+
+    PrintParking(spaces)
+
+    ParkCars(spaces, l, median - 1)
+    ParkCars(spaces, median + 1, r)
 
 
 def main():
-    parking = [False, False, False, False, False, False, False, False, False]
-    for _ in range(5):
-        s, l = get_max_slot(park2list(parking))
-        parking[s+l//2] = True
-        print(parking)
+    numberOfSpaces = input("Enter number of free spaces: ")
+    spaces = [0 for _ in range(int(numberOfSpaces))] # 0 represents an empty space
+    ParkCars(spaces, 0, len(spaces) - 1)
 
-
+    PrintParking(spaces)
 
 if __name__ == '__main__':
     main()
