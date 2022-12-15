24cae84690da1cfe120b0a8a50ffdce825b0eb4e
Giovanni Squillero <giovanni.squillero@polito.it>
2022-12-15 08:39:03 +0100

    Add December lesons
    Former-commit-id: 3448abe79ad992a5550943e1aa324f56edd09d94 [formerly 51978f21ee3112b58c451ab48a0e78d0025b714f]
    Former-commit-id: a2f36580b04dd13ec2776216c607ee8bc7f72768

---
+++ Python/src/2022-23/20221129_play-dice_take2.py
@@ -0,0 +1,52 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+import random
+from pprint import pprint
+
+NUM_RANDOM_ROLLS = 10
+
+
+def count_beginning(lst):
+    """Count repetitions in the beginning of a list"""
+    cnt = 0
+    while cnt < len(lst) and lst[0] == lst[cnt]:
+        cnt += 1
+    return cnt
+
+
+def myprint(lst):
+    out = ""
+    for e in lst:
+        out += f"{e} "
+    print(out, end='')
+
+
+def main():
+    rolls = [random.randint(1, 6) for _ in range(NUM_RANDOM_ROLLS)]
+    pprint(rolls)
+
+    reps = [(start, count_beginning(rolls[start:])) for start in range(NUM_RANDOM_ROLLS)]
+    pprint(reps)
+
+    max_ = [reps.pop(0)]
+    for r in reps:
+        if r[1] > max_[0][1]:
+            max_ = [r]
+        elif r[1] == max_[0][1]:
+            max_.append(r)
+    pprint(max_)
+
+    pos = 0
+    for rep_index, rep_length in max_:
+        myprint(rolls[pos:rep_index])
+        print("(", end=' ')
+        myprint(rolls[rep_index:rep_index + rep_length])
+        print(")", end=' ')
+        pos = rep_index + rep_length
+    myprint(rolls[pos:])
+
+
+if __name__ == '__main__':
+    main()
---
+++ Python/src/2022-23/20221206_dec2bin.py
@@ -0,0 +1,31 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+
+def dec2bin(num):
+    mods = list()
+    while num > 0:
+        mods.append(num % 2)
+        num = num // 2
+    if not mods:
+        result = '0'
+    else:
+        result = ''
+        for d in reversed(mods):
+            result += str(d)
+    return result
+
+
+def main():
+    try:
+        number = int(input("Number: "))
+    except ValueError as error:
+        print(f"Yeuch! {error}")
+        exit(1)
+    binary = dec2bin(number)
+    print(f"{number}|10 -> {binary}|2")
+
+
+if __name__ == '__main__':
+    main()
