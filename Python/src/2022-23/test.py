cd12f96b52a28ad0dd26fbfe1f65e27cd0211f53
Giovanni Squillero <giovanni.squillero@polito.it>
2022-12-16 14:04:28 +0100

    Remove spurious file
    Former-commit-id: 3b0413982a6ede18ed8e325727d023d13100c69f [formerly 5b148442eca0978cd187ff7e4ccc9e76e1c7e196]
    Former-commit-id: d421baba6a490d10b7a863781213edac71f9d32b

--- Python/src/2022-23/test.py
+++
@@ -1,7 +0,0 @@
-# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
-# https://github.com/squillero/computer-sciences
-# Free for personal or classroom use; see 'LICENSE.md' for details.
-
-a = list("Giovanni")
-
-print(a.index('i', 7 + 1))
