383abf38514055db424ac792a11ccd150be04acd
Giovanni Squillero <giovanni.squillero@polito.it>
2022-12-20 15:24:14 +0100

    Add CSV example
    Former-commit-id: 8d187202a522de951621fc9d233910b6c00f7bad [formerly a79cde18fb21464f5edb3c8c59166101dc1b8750]
    Former-commit-id: 89c0362d6ed4f3bb506552b14a2b88b7b2d4e016

---
+++ Python/src/2022-23/20221220_csv.py
@@ -0,0 +1,52 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+from pprint import pprint
+import csv
+
+FILENAME = 'test.csv'
+
+
+def read_file_csv_dict(filename):
+    data = dict()
+    try:
+        with open(filename, encoding='utf8') as raw_file:
+            for d in csv.DictReader(raw_file):
+                data[d['LANGUAGE']] = d['GREET']
+    except OSError as error:
+        print(f"Yeuch: {error}")
+    return data
+
+
+def read_file_csv(filename):
+    data = dict()
+    try:
+        with open(filename, encoding='utf8') as raw_file:
+            for lang, greet in csv.reader(raw_file):
+                data[lang] = greet
+    except OSError as error:
+        print(f"Yeuch: {error}")
+    return data
+
+
+def read_file(filename):
+    data = dict()
+    try:
+        with open(filename, encoding='utf8') as file_in:
+            for line in file_in:
+                lang, greet = line.split(',', maxsplit=1)
+                data[lang] = greet[1:-1]
+    except OSError as error:
+        print(f"Yeuch: {error}")
+    return data
+
+
+def main():
+    data = read_file_csv_dict(FILENAME)
+    for lang, greet in data.items():
+        print(f"In {lang} we say \"{greet}\"")
+
+
+if __name__ == '__main__':
+    main()
