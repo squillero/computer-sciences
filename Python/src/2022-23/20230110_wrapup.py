c74a64a2424f803f30c984b55b0551bd70c19b54
Giovanni Squillero <giovanni.squillero@polito.it>
2023-01-10 17:17:21 +0100

    Add WrapUp 2023
    Former-commit-id: ab8893971db65233a4d20540e393495fd0c45950 [formerly 6d9c9ec12dec130948f29156ebff99179fe15864]
    Former-commit-id: e923fc84d9f22f782d2d4e586ede8e9c16de0d38

---
+++ Python/src/2022-23/20230110_wrapup.py
@@ -0,0 +1,65 @@
+# Copyright © 2023 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+from pprint import pprint
+from csv import DictReader
+import random
+
+FILENAME = 'test.csv'
+
+
+def read_csv(filename):
+    info = list()
+    try:
+        with open(filename, encoding='utf8') as file_in:
+            dict_reader = DictReader(file_in)
+            for line in dict_reader:
+                info.append(line)
+    except OSError as problem:
+        exit("Yeuch!")
+    return info
+
+
+def sample2():
+    data = dict()
+    for i in range(10):
+        data[i] = random.random()
+    pprint(data)
+    print()
+
+    def key_getter(k):
+        return data[k]
+
+    pprint(sorted(data.keys(), key=key_getter))
+
+
+def sample3():
+    data = dict()
+    for i in range(10):
+        data[i] = random.random()
+    pprint(data.items())
+
+    def key_extractor(element):
+        return element[1]
+
+    for k, v in sorted(data.items(), key=key_extractor):
+        print(f"{k} -> {v}")
+
+
+def main():
+    sample3()
+    exit()
+    info = read_csv(FILENAME)
+    pprint(info)
+    print()
+
+    def key_extractor(blob):
+        return len(blob['GREET'])
+
+    info.sort(key=key_extractor, reverse=True)
+    pprint(info)
+
+
+if __name__ == '__main__':
+    main()
