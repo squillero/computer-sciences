a040044f12c46c66d26655e75aa8a85eefa6c3ad
Giovanni Squillero <giovanni.squillero@polito.it>
2023-10-20 12:55:57 +0200

    Add lecture of Friday, October 20
    Former-commit-id: f075fbf2e0bc06affbb8acbfdf58e7919f622c55 [formerly 50eaa73844cc572abadc482af8cb4a26dbf5cccf]
    Former-commit-id: 4a4816be05333405d1f36dd024305c92ad2be985

---
+++ Python/src/2023-24/20231020 sum-1.py
@@ -0,0 +1,16 @@
+# Copyright © 2023 Giovanni Squillero <giovanni.squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+NUMBER = 1732817328
+
+number = NUMBER
+total = 0
+while number > 0:
+    # print(f"Number: {number}")
+    digit = number % 10
+    # print(f"Last digit: {digit}")
+    number = number // 10
+    total = total + digit
+
+print(f"The sum of all digits of {NUMBER} is {total}")
