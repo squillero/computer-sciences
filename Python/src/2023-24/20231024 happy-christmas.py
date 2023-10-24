94dc45b1294c69a31b19c517bbd09b8b0bc8cf1f
Giovanni Squillero <giovanni.squillero@polito.it>
2023-10-24 16:45:58 +0200

    Add lecture of October 24
    Former-commit-id: 2cea327197bb83ad414248543856afd175c7bed2 [formerly 12d26a91a0abafcdad7431ce3316cdd234a207ed]
    Former-commit-id: b6508c4673bc9ad9c523eeca47ac6115a1a8711c

---
+++ Python/src/2023-24/20231024 happy-christmas.py
@@ -0,0 +1,37 @@
+# Copyright © 2023 Giovanni Squillero <giovanni.squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+from random import random
+
+TREE_CANOPY_SIZE = 10
+TREE_TRUNK_SIZE = 5
+
+for line in range(TREE_CANOPY_SIZE):
+    for row in range(TREE_CANOPY_SIZE - line - 1):
+        print(".", end="")
+    for row in range(line + 1 + line):
+        if random() < 0.15:
+            fill = "*"
+        else:
+            fill = "%"
+        print(fill, end="")
+    for row in range(TREE_CANOPY_SIZE - line - 1):
+        print(".", end="")
+    print()
+
+for line in range(TREE_TRUNK_SIZE):
+    for row in range(TREE_CANOPY_SIZE - 2):
+        print(".", end="")
+    print("###", end="")
+    for row in range(TREE_CANOPY_SIZE - 2):
+        print(".", end="")
+    print()
+
+for row in range(TREE_CANOPY_SIZE - 4):
+    print(".", end="")
+print("#######", end="")
+for row in range(TREE_CANOPY_SIZE - 4):
+    print(".", end="")
+
+print()
