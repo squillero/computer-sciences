a58f761869d1a9406bab9ea3f037d31e4b32849b
Giovanni Squillero <giovanni.squillero@polito.it>
2023-11-24 12:44:34 +0100

    Add lecture of Friday, November 24

---
+++ Python/src/2023-24/20231117 tables.py
@@ -0,0 +1,23 @@
+# Copyright © 2023 Giovanni Squillero <giovanni.squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+import warnings
+from pprint import pprint
+
+
+def make_table(rows, columns):
+    warnings.warn('Yeuch, "tables" do not exist in Python!')
+    table = list()
+    for row in range(rows):
+        table.append([None] * columns)
+    return table
+
+
+def main():
+    table = make_table(10, 5)
+    pprint(table)
+
+
+if __name__ == "__main__":
+    main()
