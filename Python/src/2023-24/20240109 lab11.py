a2395d1103e9a2c75ad198882b62e43c9fa85a5c
Giovanni Squillero <giovanni.squillero@polito.it>
2024-01-09 17:25:10 +0100

    Add lecture of January, 9 2024
    Former-commit-id: 12bf7d9486815bf3ee86de400c6edee5f9fc0bcc [formerly fb16feda7cb8b0ab41278f6d1c2a7cde5ae66d1e]
    Former-commit-id: 2c3473a059e909cb0c5119956e746b33be194bcd

---
+++ Python/src/2023-24/20240109 lab11.py
@@ -0,0 +1,55 @@
+# Copyright © 2023 Giovanni Squillero <giovanni.squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+from pprint import pprint
+
+FOLDER = "20240109 lab11"
+ARTIST_FILE = "artists.txt"
+
+
+def read_artists(filename):
+    artists = dict()
+    try:
+        with open(filename) as artists_file:
+            for line in artists_file:
+                code, data = line.split(";")
+                artists[code] = data.strip()
+    except OSError as problem:
+        exit(problem)
+
+    return artists
+
+
+def read_albums(database, filename, artist_code):
+    try:
+        with open(filename) as album_file:
+            for line in album_file:
+                year, title = line.split(";")
+                year = int(year)
+
+                if year not in database:
+                    database[year] = list()
+
+                database[year].append((title.strip(), artist_code))
+    except OSError as problem:
+        exit(problem)
+
+
+def print_database(database):
+    for year in sorted(database):
+        print(f"{year}:")
+        for title, artist in database[year]:
+            print(f"{title:<50} {artist}")
+
+
+def main():
+    artists = read_artists(FOLDER + "/" + ARTIST_FILE)
+    database = dict()
+    for code, filename in artists.items():
+        read_albums(database, FOLDER + "/" + filename, code)
+    print_database(database)
+
+
+if __name__ == "__main__":
+    main()
