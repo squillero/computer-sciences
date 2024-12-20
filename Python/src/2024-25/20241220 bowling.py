5af2b7facb649a1969c2658ba979a1a7cc32a770
Giovanni Squillero <giovanni.squillero@polito.it>
2024-12-20 11:43:20 +0100

    Add last 2024 lecture

---
+++ Python/src/2024-25/20241220 bowling.py
@@ -0,0 +1,6 @@
+# Copyright © 2024 Giovanni Squillero <giovanni.squillero@polito.it>
+# https://github.com/squillero/computer-sciences
+# Free under certain conditions — see the license for details.
+
+
+BOWLING_FILE = '20241220 bowling.txt'
