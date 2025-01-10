b3e7730b450382d060f79bd68da4139a72b5183c
Giovanni Squillero <giovanni.squillero@polito.it>
2025-01-10 13:35:11 +0100

    Last lecture

--- Python/src/2024-25/20250110 maps.py
+++ Python/src/2024-25/20250110 maps.py
@@ -4,5 +4,31 @@
 
 FILENAME = '20250110 map.dat'
 
-def read_map(filenmae):
-    ...
+
+def read_map(filename):
+    map_ = list()
+    try:
+        with open(filename) as file:
+            for line in file:
+                map_.append(list(line.rstrip()))
+    except OSError:
+        exit("Last lecture, bug off")
+    return map_
+
+
+def print_map(map_):
+    for line in map_:
+        print(' '.join(line))
+
+
+def main():
+    map_ = read_map(FILENAME)
+    print_map(map_)
+
+    map_[10][12] = '!'
+    print()
+    print_map(map_)
+
+
+if __name__ == '__main__':
+    main()
