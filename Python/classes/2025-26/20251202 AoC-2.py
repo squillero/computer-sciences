204fa95596e7eba356a44f70559ca013ab839627
Giovanni Squillero <giovanni.squillero@polito.it>
2025-12-05 12:31:06 +0100

    Add AoC day 2

--- Python/classes/2025-26/20251202 AoC-2.py
+++ Python/classes/2025-26/20251202 AoC-2.py
@@ -6,6 +6,7 @@ DATA_FILE = '20251202 AoC-2.data.txt'
 
 
 def read_file(filename):
+    f"""Usual 'read file'"""
     id_ranges = list()
     try:
         with open(filename) as file:
@@ -19,15 +20,47 @@ def read_file(filename):
     return id_ranges
 
 
+def find_divisors(number):
+    r"""Return a list with all the divisors of `number`"""
+    divisors = list()
+    for n in range(1, number // 2 + 1):
+        if number % n == 0:
+            divisors.append(n)
+    return divisors
+
+
+def is_valid_part1(id_):
+    r"""Check id validity (part 2)"""
+    if id_[: len(id_) // 2] == id_[len(id_) // 2 :]:
+        return False
+    else:
+        return True
+
+
+def is_valid_part2(id_):
+    r"""Check id_ validity (part 2)"""
+
+    for n in find_divisors(len(id_)):
+        # print(f"{id_} = {id_[:n]} repeated {len(id_)//n} times?")
+        if id_ == id_[:n] * (len(id_) // n):
+            return False
+    return True
+
+
 def main():
     check = 0
     id_ranges = read_file(DATA_FILE)
     for from_, to_ in id_ranges:
         for id_ in range(from_, to_ + 1):
-            tmp = str(id_)
-            if tmp[: len(tmp) // 2] == tmp[len(tmp) // 2 :]:
+            if not is_valid_part1(str(id_)):
                 check += id_
-                print(tmp)
+    print(f"Final check: {check}")
+
+    check = 0
+    for from_, to_ in id_ranges:
+        for id_ in range(from_, to_ + 1):
+            if not is_valid_part2(str(id_)):
+                check += id_
     print(f"Final check: {check}")
 
 
